4f9b17a254015e44b69b6c357f143061930ede40
Danilo Horta <danilo.horta@gmail.com>
2016-08-17 18:29:39 +0100

    test working

--- build_capi/test/test_examples.py
+++ build_capi/test/test_examples.py
@@ -1,6 +1,12 @@
 from subprocess import check_output
+import sys
 
 def test_build():
-    cmd = 'python examples/prj_name/setup.py build'
+    ps = [p for p in sys.path if 'build_capi' in p]
+    e = ''
+    for p in ps:
+        e += '%s:' % p
+    e = 'PYTHONPATH=' + e + '$PYTHONPATH'
+    cmd = 'env %s cd examples/prj_name/ && python setup.py build' % e
     o = check_output(cmd, shell=True)
     assert(o == 'running build\n')
--- examples/prj_name/setup.py
+++ examples/prj_name/setup.py
@@ -1,26 +1,27 @@
 from os.path import join
 from setuptools import setup
+import os
 from build_capi import add_capi_opts
-setup = add_capi_opts(setup)
-from build_capi import CApiLib
-
-mylib = CApiLib('pkg_name.lib.mylib',
-                sources=[join('pkg_name', 'sources', 'example.c')],
-                include_dirs=[join('pkg_name', 'sources')],
-                libraries=['m'],
-                library_dirs=[]
-                )
-
-
-setup_params = dict(
-    name='pkg_name',
-    author="Danilo Horta",
-    author_email="danilo.horta@gmail.com",
-    description='description',
-    long_description='long description',
-    setup_requires=['build_capi'],
-    capi_libs=[mylib]
-)
-
-if __name__ == '__main__':
-    setup(**setup_params)
+# setup = add_capi_opts(setup)
+# from build_capi import CApiLib
+#
+# mylib = CApiLib('pkg_name.lib.mylib',
+#                 sources=[join('pkg_name', 'sources', 'example.c')],
+#                 include_dirs=[join('pkg_name', 'sources')],
+#                 libraries=['m'],
+#                 library_dirs=[]
+#                 )
+#
+#
+# setup_params = dict(
+#     name='pkg_name',
+#     author="Danilo Horta",
+#     author_email="danilo.horta@gmail.com",
+#     description='description',
+#     long_description='long description',
+#     setup_requires=['build_capi'],
+#     capi_libs=[mylib]
+# )
+#
+# if __name__ == '__main__':
+#     setup(**setup_params)
