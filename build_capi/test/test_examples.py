59ad45b0336e3a6dcf0f8691d491ff92d701beb0
Danilo Horta <danilo.horta@gmail.com>
2016-08-17 17:40:32 +0100

    again

--- build_capi/test/test_examples.py
+++ build_capi/test/test_examples.py
@@ -1,15 +1,6 @@
-import os
 from subprocess import check_output
 
 def test_build():
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    src_path = os.path.abspath(dir_path)
-    old_path = os.getcwd()
-    os.chdir(src_path)
-
-    try:
-        cmd = 'python ../examples/prj_name/setup.py build'
-        o = check_output(cmd, shell=True)
-        assert(o == 'running build\n')
-    finally:
-        os.chdir(old_path)
+    cmd = 'python examples/prj_name/setup.py build'
+    o = check_output(cmd, shell=True)
+    assert(o == 'running build\n')
