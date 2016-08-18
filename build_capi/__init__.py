d943e6b97abed69f9724e8eb05dd84cb9a671e71
Danilo Horta <danilo.horta@gmail.com>
2016-08-18 11:24:37 +0100

    test

--- build_capi/__init__.py
+++ build_capi/__init__.py
@@ -1,3 +1,17 @@
 from .setup import add_capi_opts
 from .capi_lib import CApiLib
 from .build_capi import build_capi
+
+def test():
+    import os
+    p = __import__('build_capi').__path__[0]
+    src_path = os.path.abspath(p)
+    old_path = os.getcwd()
+    os.chdir(src_path)
+
+    try:
+        return_code = __import__('pytest').main([])
+    finally:
+        os.chdir(old_path)
+
+    return return_code
