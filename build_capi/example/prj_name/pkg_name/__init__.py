9ad8068fcb315604957747d24b94fba6da32d8bf
Danilo Horta <danilo.horta@gmail.com>
2016-11-07 16:14:17 +0000

    better example

--- build_capi/example/prj_name/pkg_name/__init__.py
+++ build_capi/example/prj_name/pkg_name/__init__.py
@@ -1,12 +1,9 @@
 def get_include():
     import pkg_name
     from os.path import join, dirname
-    d = join(dirname(pkg_name.__file__), 'include')
-    return d
-
+    return join(dirname(pkg_name.__file__), 'include')
 
 def get_lib():
     import pkg_name
     from os.path import join, dirname
-    d = join(dirname(pkg_name.__file__), 'lib')
-    return d
+    return join(dirname(pkg_name.__file__), 'lib')
