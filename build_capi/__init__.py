ed79f14387b487617edbddc9f3517314e7dddb7e
Danilo Horta <danilo.horta@gmail.com>
2016-11-13 08:23:27 +0000

    rename

--- build_capi/__init__.py
+++ build_capi/__init__.py
@@ -3,8 +3,8 @@ from __future__ import absolute_import as _absolute_import
 from pkg_resources import get_distribution as _get_distribution
 from pkg_resources import DistributionNotFound as _DistributionNotFound
 
-from .setuptools_ext import CApiLib
-from .build_capi import build_capi
+from ._setuptools_ext import CApiLib
+from ._build_capi import build_capi
 
 try:
     __version__ = _get_distribution('build_capi').version
