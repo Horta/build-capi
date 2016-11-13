950e01e70966951c1d515ad75a2dff4d3fdb5ec8
Danilo Horta <danilo.horta@gmail.com>
2016-11-13 09:11:45 +0000

    again

--- example/test/test_example.py
+++ example/test/test_example.py
@@ -4,9 +4,8 @@ from subprocess import check_output
 import sys
 import os
 
-def test_build():
-    path_ = __import__('build_capi').__path__[0]
-    src_path = os.path.abspath(path_)
+def test_example():
+    src_path = os.path.dirname(os.path.realpath(__file__))
     old_path = os.getcwd()
     os.chdir(src_path)
 
@@ -16,7 +15,7 @@ def test_build():
         for p in ps:
             e += '%s:' % p
         e = 'PYTHONPATH=' + e + '$PYTHONPATH'
-        cmd = 'cd ../example/prj_name/ && env %s python setup.py build' % e
+        cmd = 'cd ../prj_name/ && env %s python setup.py build' % e
         o = check_output(cmd, shell=True, universal_newlines=True)
         assert('running build' in o)
     finally:
