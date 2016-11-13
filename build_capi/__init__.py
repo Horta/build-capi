207f78af525f4cf5624ea0d4b3d4a235e1610639
Danilo Horta <danilo.horta@gmail.com>
2016-11-13 09:05:06 +0000

    test

--- build_capi/__init__.py
+++ build_capi/__init__.py
@@ -10,20 +10,3 @@ try:
     __version__ = _get_distribution('build_capi').version
 except _DistributionNotFound:
     __version__ = 'unknown'
-
-def test():
-    import os
-    p = __import__('build_capi').__path__[0]
-    src_path = os.path.abspath(p)
-    old_path = os.getcwd()
-    os.chdir(src_path)
-
-    try:
-        return_code = __import__('pytest').main(['-q'])
-    finally:
-        os.chdir(old_path)
-
-    if return_code == 0:
-        print("Congratulations. All tests have passed!")
-
-    return return_code
