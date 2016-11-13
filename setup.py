dd21260ead1f314ac501cee3ebc080b38efc2ce7
Danilo Horta <danilo.horta@gmail.com>
2016-11-13 19:50:09 +0000

    bump

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def setup_package():
 
     metadata = dict(
         name='build_capi',
-        version='1.1.5',
+        version='1.1.6',
         maintainer="Danilo Horta",
         maintainer_email="horta@ebi.ac.uk",
         description='Build and distribute C/C++ static libraries',
